5cd6f76c938644b59f4582b130acfbbb6875d13d
Balaji <balaji0017@gmail.com>
2024-08-30 14:55:02 +0530

    page object model without keywords

---
+++ src/test/java/com/cgi/pages/LeavePage.java
@@ -0,0 +1,13 @@
+package com.cgi.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LeavePage {
+
+	private WebDriver driver;
+
+	public LeavePage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
---
+++ src/test/java/com/cgi/pages/MainPage.java
@@ -0,0 +1,14 @@
+package com.cgi.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * WebElement for all common items like menu and profile handling section
+ */
+public class MainPage {
+	private WebDriver driver;
+
+	public MainPage(WebDriver driver) {
+		this.driver = driver;
+	}
+}
